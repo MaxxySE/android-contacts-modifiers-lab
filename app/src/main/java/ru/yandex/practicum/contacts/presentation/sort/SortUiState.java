ef33df019274ea69589d84a2d1638408191d18c4
MaxxySE <55539735+MaxxySE@users.noreply.github.com>
2023-10-13 18:15:04 +0400

    сделал геттеры и сеттеры

--- app/src/main/java/ru/yandex/practicum/contacts/presentation/sort/SortUiState.java
+++ app/src/main/java/ru/yandex/practicum/contacts/presentation/sort/SortUiState.java
@@ -1,11 +1,27 @@
 package ru.yandex.practicum.contacts.presentation.sort;
 
 // класс UiState должен иметь package-private доступ
-public class SortUiState {
+class SortUiState {
 
     // сделайте поля isApplyEnable и newSelectedSortType приватными
-    public boolean isApplyEnable = false;
-    public String newSelectedSortType = null;
+    private boolean isApplyEnable = false;
+    private String newSelectedSortType = null;
 
     // реализуйте get и set методы для обоих полей
+
+    public boolean getApplyEnable(){
+        return this.isApplyEnable;
+    }
+
+    public String getNewSelectedSortType(){
+        return this.newSelectedSortType;
+    }
+
+    public void setApplyEnable(boolean isApplyEnable){
+        this.isApplyEnable = isApplyEnable;
+    }
+
+    public void setNewSelectedSortType(String newSelectedSortType){
+        this.newSelectedSortType = newSelectedSortType;
+    }
 }
